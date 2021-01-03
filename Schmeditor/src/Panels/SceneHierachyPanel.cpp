46794f248f4a3d3af9df74e24c71af3282072391
Thomas Wiest <thomaswiest@hotmail.com>
2021-01-03 22:51:42 +0100

    gui stuff

--- Schmeditor/src/Panels/SceneHierachyPanel.cpp
+++ Schmeditor/src/Panels/SceneHierachyPanel.cpp
@@ -3,6 +3,7 @@
 #include <imgui/imgui.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.inl>
+#include <imgui\imgui_internal.h>
 
 namespace Schmog {
 
@@ -64,9 +65,105 @@ namespace Schmog {
 		}
 	}
 
-	void DrawVec3Control()
+	static void DrawVec3Control(const std::string& label, glm::ivec3& values, int resetValue = 0, float columnWidth = 100.0f)
 	{
+		ImGui::PushID(label.c_str());
 
+		ImGui::Columns(2);
+		ImGui::SetColumnWidth(0, columnWidth);
+		ImGui::Text(label.c_str());
+		ImGui::NextColumn();
+
+		ImGui::PushMultiItemsWidths(3, ImGui::CalcItemWidth());
+		ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{ 0, 0 });
+
+		float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
+		ImVec2 buttonSize = { lineHeight + 3.0f, lineHeight };
+
+		if (ImGui::Button("X", buttonSize))
+			values.x = resetValue;
+
+		ImGui::SameLine();
+		ImGui::InputInt("##X", &values.x, 0);
+		ImGui::PopItemWidth();
+		ImGui::SameLine();
+
+		if (ImGui::Button("Y", buttonSize))
+			values.y = resetValue;
+
+		ImGui::SameLine();
+		ImGui::InputInt("##Y", &values.y, 0);
+		ImGui::PopItemWidth();
+		ImGui::SameLine();
+
+		if (ImGui::Button("Z", buttonSize))
+			values.z = resetValue;
+
+		ImGui::SameLine();
+		ImGui::InputInt("##Z", &values.z, 0);
+		ImGui::PopItemWidth();
+
+		ImGui::PopStyleVar();
+
+		ImGui::Columns(1);
+
+		ImGui::PopID();
+	}
+
+	static void DrawVec2Control(const std::string& label, glm::ivec2& values, int resetValue = 0, float columnWidth = 100.0f)
+	{
+		ImGui::PushID(label.c_str());
+
+		ImGui::Columns(2);
+		ImGui::SetColumnWidth(0, columnWidth);
+		ImGui::Text(label.c_str());
+		ImGui::NextColumn();
+
+		ImGui::PushMultiItemsWidths(3, ImGui::CalcItemWidth());
+		ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{ 0, 0 });
+
+		float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
+		ImVec2 buttonSize = { lineHeight + 3.0f, lineHeight };
+
+		if (ImGui::Button("X", buttonSize))
+			values.x = resetValue;
+
+		ImGui::SameLine();
+		ImGui::InputInt("##X", &values.x, 0);
+		ImGui::PopItemWidth();
+		ImGui::SameLine();
+
+		if (ImGui::Button("Y", buttonSize))
+			values.y = resetValue;
+
+		ImGui::SameLine();
+		ImGui::InputInt("##Y", &values.y, 0);
+		ImGui::PopItemWidth();
+		ImGui::PopItemWidth();
+
+		ImGui::PopStyleVar();
+
+		ImGui::Columns(1);
+
+		ImGui::PopID();
+	}
+
+	static void DrawIntControl(const std::string& label, int& value, int resetValue = 0, int step = 0, float columnWidth = 100.0f)
+	{
+		ImGui::PushID(label.c_str());
+
+		ImGui::Columns(2);
+		ImGui::SetColumnWidth(0, columnWidth);
+		ImGui::Text(label.c_str());
+		ImGui::NextColumn();
+		ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{ 0, 0 });
+
+		ImGui::InputInt("##X", &value, step);
+		ImGui::PopStyleVar();
+
+		ImGui::Columns(1);
+
+		ImGui::PopID();
 	}
 
 	void SceneHierachyPanel::DrawComponents(Entity entity)
@@ -87,9 +184,9 @@ namespace Schmog {
 		DrawComponent<TransformComponent>("Transform", entity, [&](TransformComponent& component)
 			{
 				auto& trans = component;
-				ImGui::InputInt3("Translation", &(trans.Position.x));
-				ImGui::InputInt("Rotation", &(trans.RotationDeg), 500, 1000);
-				ImGui::InputInt2("Scale", &(trans.Scale.x));
+				DrawVec3Control("Translation", trans.Position);
+				DrawVec2Control("Scale", trans.Scale, 1);
+				DrawIntControl("Rotation", trans.RotationDeg, 0, 500);
 			}
 		);
 
